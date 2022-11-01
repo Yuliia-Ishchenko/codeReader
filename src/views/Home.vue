5a2522d49f337e460a66d5ea6ed6b8a5836434c9
Yuliia Ishchenko <golovinadic@gmail.com>
2022-11-01 10:25:09 +0100

    add reader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>{{testReaderCode}}</h1>
+    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
-<script lang="ts">
+<script setup>
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+import { StreamBarcodeReader } from "vue-barcode-reader";
+import { ref,} from 'vue';
+const testReaderCode = ref();
+function onLoaded(result){
+  // console.log(result);
+}
+function onDecode(result){
+  // console.log(result);
+  testReaderCode.value = result
+}
 </script>
