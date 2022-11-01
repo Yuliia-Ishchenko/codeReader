096bfe7f3d73e5c09fc49f851c6fb59551660d26
Yuliia Ishchenko <golovinadic@gmail.com>
2022-11-01 11:55:54 +0100

    add sound

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,7 @@ import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import { StreamBarcodeReader } from "vue-barcode-reader";
 import { ref,} from 'vue';
+import sound from '@/assets/sounds/beep-07a.mp3'
 const testReaderCode = ref();
 function onLoaded(result){
   // console.log(result);
@@ -18,5 +19,7 @@ function onLoaded(result){
 function onDecode(result){
   // console.log(result);
   testReaderCode.value = result
+  var audio = new Audio(sound)
+  audio.play()
 }
 </script>
